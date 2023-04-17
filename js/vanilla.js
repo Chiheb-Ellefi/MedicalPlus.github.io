ecce32ac59aa1ee4a9b613de68c1de638ba26c1a
Your Name <you@example.com>
2023-04-17 17:15:08 +0100

    committed

---
+++ js/vanilla.js
@@ -0,0 +1,30 @@
+function btncolor(clicked) {
+    document.getElementById(clicked).style.color = '#7b2cbf'
+}
+
+var button = document.getElementById('slide');
+button.onclick = function() {
+    var container = document.getElementById('container');
+    sideScroll(container, 'right', 25, 100, 10);
+};
+
+var back = document.getElementById('slideBack');
+back.onclick = function() {
+    var container = document.getElementById('container');
+    sideScroll(container, 'left', 25, 100, 10);
+};
+
+function sideScroll(element, direction, speed, distance, step) {
+    scrollAmount = 0;
+    var slideTimer = setInterval(function() {
+        if (direction == 'left') {
+            element.scrollLeft -= step;
+        } else {
+            element.scrollLeft += step;
+        }
+        scrollAmount += step;
+        if (scrollAmount >= distance) {
+            window.clearInterval(slideTimer);
+        }
+    }, speed);
+}
